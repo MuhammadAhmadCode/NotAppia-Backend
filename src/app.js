d46422284e7a5c5ddf789d6bf0c43fd2fad13a9e
MuhammadAhmadCode <ma2565983@gmail.com>
2026-05-03 16:22:58 +0500

    deleted dotenv

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
-require("dotenv").config()
 const express = require("express");
-const cors = require("cors")
-const noteRoutes = require("./routes/note.routes")
-const authRoutes = require("./routes/auth.routes")
-const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const noteRoutes = require("./routes/note.routes");
+const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
-
-
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.Frontend_URI,
-    credentials: true
-  }));
-  
-app.use(cookieParser())
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
 
 //auth routes
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
 //notes routes
-app.use("/api/notes",noteRoutes)
+app.use("/api/notes", noteRoutes);
 
-
-
-module.exports = app
+module.exports = app;
